007a2d83eeb86009864f4c3d24e90f59d23006d6
无名牌 <2134868121@qq.com>
2025-12-27 22:00:52 +0800

    新增: 更新UI库

--- src/com/wmp/PublicTools/CTInfo.java
+++ src/com/wmp/PublicTools/CTInfo.java
@@ -52,7 +52,7 @@ public class CTInfo {
      * d:只修复的问题,问题较少<br>
      * e:测试版本号
      */
-    public static String version = "1.48.2.0.1";
+    public static String version = "1.48.3";
 
     static {
         init();
